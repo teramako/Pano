6e8016faf6c67e6bb79b9a5f2e5ce50ea0806d3a
teramako <teramako@gmail.com>
2011-06-25 20:27:10 +0900

    move pano tree utilties to new sub-script

---
+++ chrome/content/pano-tree.sub.js
@@ -0,0 +1,116 @@
+
+
+function onDragStart (aEvent) {
+  PanoramaTreeView.onDragStart(aEvent, view);
+}
+
+function selectTab (aEvent) {
+  var index = view.selection.currentIndex;
+  if (index == -1)
+    return;
+
+  var item = view.rows[index];
+  if (item.type & TAB_ITEM_TYPE) {
+    if (aEvent) {
+      aEvent.preventDefault();
+      aEvent.stopPropagation();
+    }
+    gBrowser.mTabContainer.selectedIndex = item.tab._tPos;
+  }
+}
+
+function newGroup () {
+  gWin.TabView._window.GroupItems.newGroup().newTab();
+}
+
+function setFilter (aString) {
+  view.setFilter(aString ? aString : null);
+}
+
+var tooltip = {
+  get titleElm () {
+    var elm = document.getElementById("panoTreeTooltipTitle");
+    delete this.titleElm;
+    return this.titleElm = elm;
+  },
+  get urlElm () {
+    var elm = document.getElementById("panoTreeTooltipURL");
+    delete this.urlElm;
+    return this.urlElm = elm;
+  },
+  get imageElm () {
+    var elm = document.getElementById("panoTreeTooltipImage");
+    delete this.imageElm;
+    return this.imageElm = elm;
+  },
+  build: function buildTooltip (aEvent) {
+    aEvent.stopPropagation();
+    var item = view.getItemFromEvent(aEvent);
+    if (!item || !(item.type & TAB_ITEM_TYPE)) {
+      aEvent.preventDefault();
+      return;
+    }
+
+    this.titleElm.setAttribute("value", item.title);
+    this.urlElm.setAttribute("value", item.url);
+    this.imageElm.classList.remove("hide");
+    var browser = item.tab.linkedBrowser;
+    if (browser.__SS_restoreState) {
+      if (item.tab._tabViewTabItem)
+        document.mozSetImageElement("panoTabCapture", item.tab._tabViewTabItem.$cachedThumb[0]);
+      else
+        this.imageElm.classList.add("hide");
+    } else {
+      document.mozSetImageElement("panoTabCapture", browser);
+    }
+  },
+};
+
+var contextMenu = {
+  get groupCloseElm () {
+    var elm = document.getElementById("panoContextMenu_groupClose");
+    delete this.groupCloseElm;
+    return this.groupCloseElm = elm;
+  },
+  get tabCloseElm () {
+    var elm = document.getElementById("panoContextMenu_tabClose");
+    delete this.tabCloseElm;
+    return this.tabCloseElm= elm;
+  },
+  currentItem: null,
+  build: function buildContextMenu (aEvent) {
+    var item = this.currentItem = view.getItemFromEvent(aEvent);
+    if (item) {
+      let isTabItem = (item.type & TAB_ITEM_TYPE) > 0;
+      let isNormalGroup = item.type == TAB_GROUP_TYPE;
+      this.showItem("groupCloseElm",isNormalGroup);
+      this.showItem("tabCloseElm", isTabItem);
+    } else {
+      this.showItem("groupCloseElm", false);
+      this.showItem("tabCloseElm", false);
+    }
+  },
+  showItem: function showMenuItem (aID, aShow) {
+    this[aID].hidden = !aShow;
+  },
+  onPopupHiding: function onContextMenuHidden () {
+    this.currentItem = null;
+  },
+  closeItem: function PT_closeItemFromContextMenu () {
+    var item = this.currentItem;
+    if (!item)
+      return;
+
+    var isTabItem = (item.type & TAB_ITEM_TYPE) > 0;
+    if (isTabItem)
+      gBrowser.removeTab(item.tab);
+    else if (item.type == TAB_GROUP_TYPE) {
+      item.group._children.forEach(function(tabItem) {
+        gBrowser.removeTab(tabItem.tab);
+      });
+      item.group.close({ immediately: true });
+    }
+  },
+};
+
+// vim: sw=2 ts=2 et:
--- chrome/content/sidebar.js
+++ chrome/content/sidebar.js
@@ -12,7 +12,7 @@ Cu.import("resource://gre/modules/XPCOMUtils.jsm");
  * @namespace
  * @name Serivces
  */
-// Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
 
 /**
  * @type {Window}
@@ -23,9 +23,11 @@ var gWin = window.top;
  * @namespace
  */
 var gPanoramaTree = {
+  gBrowser: gWin.gBrowser,
   init: function PT_init () {
     Cu.import("resource://pano/panoramaTree.jsm", this);
     gWin.TabView._initFrame(this.tabViewCallback.bind(this));
+    Services.scriptloader.loadSubScript("chrome://pano/content/pano-tree.sub.js", this);
   },
   destroy: function PT_destroy () {
     this.view.destroy();
@@ -34,106 +36,6 @@ var gPanoramaTree = {
   tabViewCallback: function PT_tabViewCallback () {
     this.view = new this.PanoramaTreeView(gWin);
     this.tree.view = this.view;
-  },
-  onDblClick: function PT_onDblClick (aEvent) {
-    var item = this.view.rows[this.view.selection.currentIndex];
-    if (item.type & this.TAB_ITEM_TYPE) {
-      aEvent.preventDefault();
-      aEvent.stopPropagation();
-      gWin.gBrowser.mTabContainer.selectedIndex = item.tab._tPos;
-    }
-  },
-  onDragStart: function PT_onDragStart (aEvent) {
-    this.PanoramaTreeView.onDragStart(aEvent, this.view);
-  },
-  tooltip: {
-    show: function PS_showTooltip (aEvent) {
-      aEvent.stopPropagation();
-      var item = gPanoramaTree.view.getItemFromEvent(aEvent);
-
-      if (!item || !(item.type & gPanoramaTree.TAB_ITEM_TYPE)) {
-        aEvent.preventDefault();
-        return;
-      }
-
-      this.titleElm.setAttribute("value", item.title);
-      this.urlElm.setAttribute("value", item.url);
-      this.imageElm.classList.remove("hide")
-      var browser = item.tab.linkedBrowser;
-      if (browser.__SS_restoreState) {
-        if (item.tab._tabViewTabItem)
-          document.mozSetImageElement("tabCapture", item.tab._tabViewTabItem.$cachedThumb[0]);
-        else
-          this.imageElm.classList.add("hide");
-      } else {
-        document.mozSetImageElement("tabCapture", browser);
-      }
-    },
-  },
-  select: function PT_select () {
-    var index = this.view.selection.currentIndex;
-    if (index > 0 && this.view.rows[index].type & this.TAB_ITEM_TYPE) {
-      gWin.gBrowser.mTabContainer.selectedIndex = this.view.rows[index].tab._tPos;
-    }
-  },
-  newGroup: function PT_newGroup () {
-    gWin.TabView._window.GroupItems.newGroup().newTab();
-  },
-  contextMenu: (function () {
-    function lazyGetter (aProperty, aGetter) {
-      XPCOMUtils.defineLazyGetter(pub, aProperty, aGetter);
-    }
-    var currentItem = null;
-    var menus = [
-      "menu_newGroup",
-      "menu_groupClose",
-      "menu_sidebarClose",
-      "menu_tabClose",
-    ];
-    var public = {
-      build: function PT_buildContextMenu (aEvent) {
-        currentItem = gPanoramaTree.view.getItemFromEvent(aEvent);
-        if (currentItem) {
-          let isTabItem = (currentItem.type & gPanoramaTree.TAB_ITEM_TYPE) > 0;
-          let isNormalGroup = currentItem.type == gPanoramaTree.TAB_GROUP_TYPE;
-          this.showItem("menu_groupClose",isNormalGroup);
-          this.showItem("menu_tabClose", isTabItem);
-        } else {
-          this.showItem("menu_groupClose", false);
-          this.showItem("menu_tabClose", false);
-        }
-      },
-      showItem: function PT_showMenuItem (aID, aShow) {
-        this[aID].hidden = !aShow;
-      },
-      onPopupHiding: function PT_onContextMenuHidden () {
-        currentItem = null;
-      },
-      closeItem: function PT_closeItemFromContextMenu () {
-        if (!currentItem)
-          return;
-
-        var isTabItem = (currentItem.type & gPanoramaTree.TAB_ITEM_TYPE) > 0;
-        if (isTabItem)
-          gWin.gBrowser.removeTab(currentItem.tab);
-        else if (currentItem.type == gPanoramaTree.TAB_GROUP_TYPE) {
-          currentItem.group._children.forEach(function(tabItem) {
-            gWin.gBrowser.removeTab(tabItem.tab);
-          });
-          currentItem.group.close({ immediately: true });
-        }
-      },
-    };
-    for each (let val in menus) {
-      // see: https://bugzilla.mozilla.org/show_bug.cgi?id=449811
-      let id = val;
-      XPCOMUtils.defineLazyGetter(public, id, function () document.getElementById(id));
-    }
-
-    return public;
-  }()),
-  search: function PT_search (str) {
-    this.view.setFilter(str ? str : null);
   },
 };
 
@@ -144,22 +46,4 @@ var gPanoramaTree = {
  */
 XPCOMUtils.defineLazyGetter(gPanoramaTree, "tree", function () document.getElementById("tabGroupTree"));
 
-/**
- * @name titleElm
- * @memberof gPanoramaTree.tooltip
- * @type {Element}
- */
-XPCOMUtils.defineLazyGetter(gPanoramaTree.tooltip, "titleElm", function() document.getElementById("tooltipTitle"));
-/**
- * @name urlElm
- * @memberof gPanoramaTree.tooltip
- * @type {Element}
- */
-XPCOMUtils.defineLazyGetter(gPanoramaTree.tooltip, "urlElm", function() document.getElementById("tooltipURL"));
-/**
- * @name imageElm
- * @memberof gPanoramaTree.tooltip
- * @type {Element}
- */
-XPCOMUtils.defineLazyGetter(gPanoramaTree.tooltip, "imageElm", function() document.getElementById("tooltipImage"));
 

ab07d0f273e712dc31b316a439f4c95538e9ca2f
teramako <teramako@gmail.com>
2011-08-04 20:14:04 +0900

    FeatureImplement: Hide Tabbar when openning Pano sidebar. For #19.

--- chrome/content/sidebar.js
+++ chrome/content/sidebar.js
@@ -28,14 +28,47 @@ var gPanoramaTree = {
     Cu.import("resource://pano/panoramaTree.jsm", this);
     gWin.TabView._initFrame(this.tabViewCallback.bind(this));
     Services.scriptloader.loadSubScript("chrome://pano/content/pano-tree.sub.js", this);
+    this.tabbar.init();
   },
   destroy: function PT_destroy () {
     this.view.destroy();
+    if (this.tabbar.pref && this.tabbar.toolbar.collapsed) {
+      this.tabbar.toolbar.collapsed = false;
+    }
   },
   view: null,
   tabViewCallback: function PT_tabViewCallback () {
     this.view = new this.PanoramaTreeView(gWin);
     this.tree.view = this.view;
+  },
+  tabbar: {
+    init: function () {
+      this.checkBox.checked = this.pref;
+      this.toggleHide();
+    },
+    get toolbar () {
+      var t = gWin.document.getElementById("TabsToolbar");
+      delete this.toolbar;
+      return this.toolbar = t;
+    },
+    get pref () {
+      return Services.prefs.getBoolPref("extensions.pano.autoHideTabbar");
+    },
+    set pref (val) {
+      val = !!val;
+      Services.prefs.setBoolPref("extensions.pano.autoHideTabbar", val);
+      return val;
+    },
+    get checkBox () {
+      var elm = document.getElementById("hideTabbarCheck");
+      delete this.checkBox;
+      return this.checkBox = elm;
+    },
+    toggleHide: function () {
+      var bool = this.checkBox.checked;
+      this.pref = bool;
+      gWin.document.getElementById("TabsToolbar").collapsed = bool;
+    },
   },
 };
 

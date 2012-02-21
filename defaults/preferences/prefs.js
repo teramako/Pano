cd57184230c2dc81e561d5cdb43391cf8eb66d3b
teramako <teramako@gmail.com>
2012-02-21 20:44:51 +0900

    select the current tab's (or group's) row when the current tab is changed. For #70
     * add pref `extensions.pano.select_currenttab' (default: true)
     * change getCurrentTabAndIndex method
       returns [the group item, the group index] when the current group is collapase

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -5,4 +5,5 @@ pref("extensions.pano.showTabNumber", false);
 pref("extensions.pano.panel.autoCloseByTabSelect", false);
 pref("extensions.pano.tooltip.showThumbnail", true);
 pref("extensions.pano.tooltip.showTitle", true);
+pref("extensions.pano.select_currenttab", true);
 
